40f38ef8aaf99f061f9220eb75fdb684d0df195c
pathan-hameed <abdulhameedkhan646.ahk@gmail.com>
2025-06-07 07:34:36 +0530

    optimised the code

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -4,6 +4,123 @@ import { Link } from "react-router-dom"; // Import Link
 import "./Homepage.css";
 
 export default function Homepage() {
+  const reason = [
+    {
+      id: 1,
+      title: "Learn Real Coding Skills",
+      text: "You’ll work on real projects, which is a great way to learn and improve your coding skills.",
+    },
+    {
+      id: 2,
+      title: "Build Your Resume",
+      text: " GSoC looks amazing on your resume and can help you land jobs or internships in the future.",
+    },
+    {
+      id: 3,
+      title: "Contribute to the World",
+      text: "The code you write will be used by people around the world—it’s a chance to make a difference!",
+    },
+  ];
+
+  const domains = [
+    {
+      title: "Artificial Intelligence & Machine Learning",
+      cards: [
+        {
+          cardtitle: "TensorFlow",
+          src: "TensorFlow.png",
+          text: "Google’s open-source machine learning framework.",
+        },
+        {
+          cardtitle: "OpenCV",
+          src: "opencv.png",
+          text: "Open-source computer vision and image processing.",
+        },
+        {
+          cardtitle: "SciKit-Learn",
+          src: "scikit-learn-logo.png",
+          text: "Machine learning for Python.",
+        },
+        {
+          cardtitle: "ML4SCI (Machine Learning for Science)",
+          src: "ml4sci.png",
+          text: "AI/ML applications in scientific research.",
+        },
+      ],
+    },
+
+    {
+      title: "Web Development & Frameworks",
+      cards: [
+        {
+          cardtitle: "Django Software Foundation",
+          src: "Django_logo.png",
+          text: "Python’s leading web framework.",
+        },
+        {
+          cardtitle: "Ruby on Rails",
+          src: "Ruby_on_Rails-Logo.png",
+          text: "Open-source computer vision and image processing.",
+        },
+        {
+          cardtitle: "Rocket.Chat",
+          src: "rocket-chat.png",
+          text: "Open-source chat platform like Slack",
+        },
+        {
+          cardtitle: "Apache Software Foundation",
+          src: "Apache_Software_Foundation_Logo.png",
+          text: " AI/ML applications in scientific research.",
+        },
+      ],
+    },
+
+    {
+      title: "Scientific Computing & Data Science",
+      cards: [
+        {
+          cardtitle: "NumFOCUS",
+          src: "numfocus.png",
+          text: "Supports projects like NumPy, SciPy, pandas, and Jupyter",
+        },
+        {
+          cardtitle: "SunPy",
+          src: "sunpy_logo.png",
+          text: "Python for solar physics research.",
+        },
+        {
+          cardtitle: "Open Data Cube",
+          src: "odc.png",
+          text: "Analyzing satellite imagery and geospatial data.",
+        },
+        {
+          cardtitle: "OSGeo (Open Source Geospatial Foundation)",
+          src: "OSGeo_logo.png",
+          text: "Geographic information systems (GIS).",
+        },
+      ],
+    },
+  ];
+
+  const worksec = [
+    {
+      title: "1. Open Source Organizations :",
+      text: "Many organizations (like WordPress, Apache, or even smaller coding communities) participate in GSoC. They list projects they need help with.",
+    },
+    {
+      title: "2. Student Apply : ",
+      text: "If you’re a student (or a beginner in coding), you can apply to work on one of these projects. You don’t need to be an expert—just passionate about learning and contributing!",
+    },
+    {
+      title: "3. Mentors Guide You : ",
+      text: "If you’re selected, you’ll be paired with mentors (experienced developers) who will guide you throughout the project.",
+    },
+    {
+      title: "4. Work Remotely :",
+      text: "You work from home or anywhere you like, and you get paid by Google for completing your project.",
+    },
+  ];
+
   return (
     <div id="homepage">
       <div className="main-section" id="main-section">
@@ -30,44 +147,23 @@ export default function Homepage() {
         <div className="sub-heading">
           <h1>Why Should You Care?</h1>
         </div>
+
         <div className="reasons">
           <div className="reason-section1">
             <ul>
-              <li>
-                <div className="card">
-                  <div className="card-body">
-                    <h5 className="card-title">Learn Real Coding Skills</h5>
-                    <p className="card-text">
-                      You’ll work on real projects, which is a great way to
-                      learn and improve your coding skills.
-                    </p>
+              {reason.map((card) => (
+                <li>
+                  <div className="card">
+                    <div className="card-body" key={card.id}>
+                      <h5 className="card-title">{card.title}</h5>
+                      <p className="card-text">{card.text}</p>
+                    </div>
                   </div>
-                </div>
-              </li>
-              <li>
-                <div className="card">
-                  <div className="card-body">
-                    <h5 className="card-title">Build Your Resume</h5>
-                    <p className="card-text">
-                      GSoC looks amazing on your resume and can help you land
-                      jobs or internships in the future.
-                    </p>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div className="card">
-                  <div className="card-body">
-                    <h5 className="card-title">Contribute to the World</h5>
-                    <p className="card-text">
-                      The code you write will be used by people around the
-                      world—it’s a chance to make a difference!
-                    </p>
-                  </div>
-                </div>
-              </li>
+                </li>
+              ))}
             </ul>
           </div>
+
           <div className="reason-section2">
             <ul>
               <div className="card">
@@ -94,224 +190,44 @@ export default function Homepage() {
             million lines of code for 800+ open source organizations.
           </p>
           <p className="home-links">
-           New to GSoC? {" "}
-            <a href="https://youtu.be/Wxjxwx7mqaI?si=3K_cLzJZA9YbPKWf">Introduction to Google Summer Of Code.</a>
+            New to GSoC?{" "}
+            <a href="https://youtu.be/Wxjxwx7mqaI?si=3K_cLzJZA9YbPKWf">
+              Introduction to Google Summer Of Code.
+            </a>
           </p>
         </div>
       </div>
 
       <div className="organizations">
         <h1 className="section-title">Mentor Organisations</h1>
-
-        <div className="domain-section">
-          <h3 className="domain-title">
-            Artificial Intelligence & Machine Learning
-          </h3>
-          <div className="row row-cols-1 row-cols-md-2 g-4">
-            <div className="col">
-              <div className="card">
-                <img
-                  src="TensorFlow.png"
-                  className="card-img-top"
-                  alt="TensorFlow logo"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">TensorFlow</h5>
-                  <p className="card-text">
-                    {" "}
-                    Google’s open-source machine learning framework.
-                  </p>
+        {domains.map((domain, index) => (
+          <div className="domain-section" key={index}>
+            <h3 className="domain-title">{domain.title}</h3>
+            <div className="row row-cols-1 row-cols-md-2 g-4">
+              {domain.cards.map((card, index) => (
+                <div className="col">
+                  <div className="card" key={index}>
+                    <img
+                      src={card.src}
+                      className="card-img-top"
+                      alt="TensorFlow logo"
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{card.cardtitle}</h5>
+                      <p className="card-text"> {card.text}</p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card">
-                <img
-                  src="opencv.png"
-                  className="card-img-top"
-                  alt="OpenCV logo"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">OpenCV</h5>
-                  <p className="card-text">
-                    {" "}
-                    Open-source computer vision and image processing.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card">
-                <img
-                  src="scikit-learn-logo.png"
-                  className="card-img-top"
-                  alt="SciKit-Learn logo"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">SciKit-Learn</h5>
-                  <p className="card-text"> Machine learning for Python.</p>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card">
-                <img
-                  src="ml4sci.png"
-                  className="card-img-top"
-                  alt="blender logo"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    ML4SCI (Machine Learning for Science){" "}
-                  </h5>
-                  <p className="card-text">
-                    {" "}
-                    AI/ML applications in scientific research.
-                  </p>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
-        </div>
-
-        <div className="domain-section">
-          <h3 className="domain-title">Web Development & Frameworks</h3>
-          <div className="row row-cols-1 row-cols-md-2 g-4">
-            <div className="col">
-              <div className="card">
-                <img
-                  src="Django_logo.png"
-                  className="card-img-top"
-                  alt="Django Software Foundation logo"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Django Software Foundation</h5>
-                  <p className="card-text"> Python’s leading web framework.</p>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card">
-                <img
-                  src="Ruby_on_Rails-Logo.png"
-                  className="card-img-top"
-                  alt="Ruby on Rails logo"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Ruby on Rails</h5>
-                  <p className="card-text">
-                    {" "}
-                    Open-source computer vision and image processing.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card">
-                <img
-                  src="rocket-chat.png"
-                  className="card-img-top"
-                  alt="Rocket.Chat logo"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Rocket.Chat</h5>
-                  <p className="card-text">
-                    {" "}
-                    Open-source chat platform like Slack
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card">
-                <img
-                  src="Apache_Software_Foundation_Logo.png"
-                  className="card-img-top"
-                  alt="Apache Software Foundation logo"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Apache Software Foundation </h5>
-                  <p className="card-text">
-                    {" "}
-                    AI/ML applications in scientific research.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="domain-section">
-          <h3 className="domain-title">Scientific Computing & Data Science</h3>
-          <div className="row row-cols-1 row-cols-md-2 g-4">
-            <div className="col">
-              <div className="card">
-                <img
-                  src="numfocus.png"
-                  className="card-img-top"
-                  alt="NumFOCUS logo"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">NumFOCUS</h5>
-                  <p className="card-text">
-                    {" "}
-                    Supports projects like NumPy, SciPy, pandas, and Jupyter
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card">
-                <img
-                  src="sunpy_logo.png"
-                  className="card-img-top"
-                  alt="SunPy logo"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">SunPy</h5>
-                  <p className="card-text">
-                    {" "}
-                    Python for solar physics research.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card">
-                <img
-                  src="odc.png"
-                  className="card-img-top"
-                  alt="Open Data Cube logo"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">Open Data Cube</h5>
-                  <p className="card-text">
-                    {" "}
-                    Analyzing satellite imagery and geospatial data.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col">
-              <div className="card">
-                <img
-                  src="OSGeo_logo.png"
-                  className="card-img-top"
-                  alt="OSGeo logo"
-                />
-                <div className="card-body">
-                  <h5 className="card-title">
-                    OSGeo (Open Source Geospatial Foundation)
-                  </h5>
-                  <p className="card-text">
-                    {" "}
-                    Geographic information systems (GIS).
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <p className="home-links">What is Open Source Organisation? <a href="https://www.youtube.com/@GoogleDevelopers/videos">Learn Here</a></p>
+        ))}
+        <p className="home-links">
+          What is Open Source Organisation?{" "}
+          <a href="https://www.youtube.com/@GoogleDevelopers/videos">
+            Learn Here
+          </a>
+        </p>
       </div>
 
       <div className="working-section">
@@ -319,36 +235,12 @@ export default function Homepage() {
           <h1>How Does It Works?</h1>
         </div>
         <ol>
-          <li>
-            <h5 className="work-title">1. Open Source Organizations :</h5>
-            <p className="work-text">
-              Many organizations (like WordPress, Apache, or even smaller coding
-              communities) participate in GSoC. They list projects they need
-              help with.
-            </p>
-          </li>
-          <li>
-            <h5 className="work-title">2. Student Apply : </h5>
-            <p className="work-text">
-              If you’re a student (or a beginner in coding), you can apply to
-              work on one of these projects. You don’t need to be an expert—just
-              passionate about learning and contributing!
-            </p>
-          </li>
-          <li>
-            <h5 className="work-title">3. Mentors Guide You : </h5>
-            <p className="work-text">
-              If you’re selected, you’ll be paired with mentors (experienced
-              developers) who will guide you throughout the project.
-            </p>
-          </li>
-          <li>
-            <h5 className="work-title">4. Work Remotely :</h5>
-            <p className="work-text">
-              You work from home or anywhere you like, and you get paid by
-              Google for completing your project.
-            </p>
-          </li>
+          {worksec.map((work, index) => (
+            <li key={index}>
+              <h5 className="work-title">{work.title}</h5>
+              <p className="work-text">{work.text}</p>
+            </li>
+          ))}
         </ol>
       </div>
 
@@ -375,8 +267,17 @@ export default function Homepage() {
         </div>
       </div>
       <div className="last-link">
-        <p className="home-links">How to Apply? <a href="https://youtu.be/YN7uGCg5vLg?si=jg7XxJ84zPzPpdgd">Learn Here</a></p>
-        <p className="home-links"> For GSoC related refferals, Success stories video links and more...Visit   <Link to="/help">Help Section</Link></p>
+        <p className="home-links">
+          How to Apply?{" "}
+          <a href="https://youtu.be/YN7uGCg5vLg?si=jg7XxJ84zPzPpdgd">
+            Learn Here
+          </a>
+        </p>
+        <p className="home-links">
+          {" "}
+          For GSoC related refferals, Success stories video links and
+          more...Visit <Link to="/help">Help Section</Link>
+        </p>
       </div>
     </div>
   );
